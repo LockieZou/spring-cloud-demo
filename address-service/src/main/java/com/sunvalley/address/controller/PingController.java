3726a92dfe0e26ca53121a176a43740be942e9a7
邹细良 <zouxiliang@dstcar.com>
2019-07-19 15:01:51 +0800

    各个服务增加ping接口

---
+++ address-service/src/main/java/com/sunvalley/address/controller/PingController.java
@@ -0,0 +1,23 @@
+package com.sunvalley.address.controller;
+
+import com.sunvalley.common.vo.BaseReturnVO;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author EDZ
+ */
+@RestController
+@RequestMapping("/")
+public class PingController {
+
+	/**
+	 * ping 接口
+	 * @return
+	 */
+	@RequestMapping("/ping")
+	public BaseReturnVO ping() {
+		return new BaseReturnVO("pong");
+	}
+
+}
